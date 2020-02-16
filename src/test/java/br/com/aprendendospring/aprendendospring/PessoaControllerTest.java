2fd56a1aa146377c10ca4f2d43f5e8ae26dddceb
Paulo Hainosz <phshainosz@gmail.com>
2020-02-16 00:13:08 -0300

    testes da api de pessoas

---
+++ src/test/java/br/com/aprendendospring/aprendendospring/PessoaControllerTest.java
@@ -0,0 +1,70 @@
+package br.com.aprendendospring.aprendendospring;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.aprendendospring.model.Pessoa;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class PessoaControllerTest {
+
+	@Autowired
+	protected TestRestTemplate rest;
+
+	private ResponseEntity<Pessoa> getPessoa(String url) {
+		return rest.getForEntity(url, Pessoa.class);
+	}
+
+	private ResponseEntity<List<Pessoa>> getPessoas(String url) {
+		return rest.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Pessoa>>() {
+		});
+	}
+
+	private ResponseEntity<Pessoa> postPessoa(String url, Pessoa pessoa) {
+		return rest.postForEntity(url, pessoa, null);
+	}
+
+	@Test
+	public void teste1() {
+		List<Pessoa> pessoas = getPessoas("/pessoas").getBody();
+		assertTrue(pessoas.isEmpty());
+
+		Pessoa pessoa = getPessoa("/pessoa/1").getBody();
+		assertTrue(pessoa.getId() == null);
+	}
+
+	@Test
+	public void teste2() {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setIdade(27);
+		pessoa.setNome("Paulo");
+		pessoa.setNascimento(LocalDate.parse("1993-01-19"));
+		ResponseEntity<Pessoa> pessoaResponse = postPessoa("/pessoas", pessoa);
+
+		assertEquals(HttpStatus.CREATED, pessoaResponse.getStatusCode());
+
+		String location = pessoaResponse.getHeaders().get("Location").get(0);
+		Pessoa pessoaInserida = getPessoa(location).getBody();
+
+		assertNotNull(pessoaInserida);
+		assertEquals(pessoaInserida.getId(), 1L);
+
+		rest.delete(location);
+	}
+}
