02a0657ca3a1f044edc6995d430c579eaf6abcd1
Paulo Hainosz <phshainosz@gmail.com>
2020-02-16 02:12:00 -0300

    criado classe de mensagem de exceções

--- src/main/java/br/com/aprendendospring/api/exception/ExceptionConfig.java
+++ src/main/java/br/com/aprendendospring/api/exception/ExceptionConfig.java
@@ -1,15 +1,26 @@
 package br.com.aprendendospring.api.exception;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionConfig {
+public class ExceptionConfig extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({ EmptyResultDataAccessException.class, RuntimeException.class })
 	public ResponseEntity<?> errorNotFound(Exception e) {
 		return ResponseEntity.notFound().build();
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new Mensagem("Operação não permitida", HttpStatus.METHOD_NOT_ALLOWED.toString()));
 	}
 }
---
+++ src/main/java/br/com/aprendendospring/api/exception/Mensagem.java
@@ -0,0 +1,29 @@
+package br.com.aprendendospring.api.exception;
+
+public class Mensagem {
+
+	private String mensagem;
+	private String httpStatus;
+
+	public Mensagem(String mensagem, String httpStatus) {
+		this.mensagem = mensagem;
+		this.httpStatus = httpStatus;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(String httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+
+}
